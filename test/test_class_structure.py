326b3f0afa96216be3be2cb5b330d66174831290
Michael Kamau <michael.kamau@andela.com>
2016-11-15 15:56:12 +0300

    Add test Fellow subclasses Person

--- test/test_class_structure.py
+++ test/test_class_structure.py
@@ -1,6 +1,7 @@
 import unittest
 from office_space_allocation import staff
 from office_space_allocation import person
+from office_space_allocation import fellow
 
 class TestOfficeSpaceAllocationClassStructure(unittest.TestCase):
     """
@@ -13,3 +14,10 @@ class TestOfficeSpaceAllocationClassStructure(unittest.TestCase):
         """
         self.staff = staff.Staff()
         self.assertIsInstance(self.staff, person.Person, msg="Staff should be a subclass of Person")
+
+    def test_fellow_is_subclass_of_person(self):
+        """
+        Fellow should be a subclass of the Person class
+        """
+        self.f1 = fellow.Fellow()
+        self.assertIsInstance(self.f1, person.Person, msg="Fellow should be a subclass of Person")

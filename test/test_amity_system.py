a64c6b1a8c03e379493cc6432d3b0d99ee167f09
Michael Kamau <michael.kamau@andela.com>
2016-11-17 05:21:06 +0300

    Add test should add Person to list of Persons

---
+++ test/test_amity_system.py
@@ -0,0 +1,26 @@
+import unittest
+from office_space_allocation import amity
+from office_space_allocation import fellow
+from office_space_allocation import staff
+
+
+class TestAmitySystem(unittest.TestCase):
+    """
+    Tests for the functionality the major components of the system
+    """
+
+    def setUp(self):
+        self.amity = amity.Amity()
+
+    def test_can_add_person_to_list(self):
+        """
+        Should be able to add Person to list of person
+        """
+        p1 = fellow.Fellow("New", "Guy")
+        p2 = staff.Staff("New", 'Staff')
+        self.amity.add_person(self.p1)
+        self.amity.add_person(self.p2)
+        self.assertTupleEqual(
+            (self.amity.all_persons[0], self.amity.all_persons[1]),
+            (p1, p2)
+        )

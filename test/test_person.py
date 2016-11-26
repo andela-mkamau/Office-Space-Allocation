80ed6afa43987dbd921ec12e83d140f94160ac1c
Michael Kamau <michael.kamau@andela.com>
2016-11-26 18:44:18 +0300

    Add test equality comparison between two Fellows

--- test/test_person.py
+++ test/test_person.py
@@ -1,5 +1,5 @@
 import unittest
-from office_space_allocation import person
+from office_space_allocation import person, fellow, staff
 
 
 class TestPersonClass(unittest.TestCase):
@@ -13,3 +13,17 @@ class TestPersonClass(unittest.TestCase):
         """
         p1 = person.Person("mike", "kamau")
         self.assertEqual("Mike Kamau", p1.get_full_name())
+
+    def test_can_perfrom_equality_comparison_btn_fellows(self):
+        """
+        Should be able to do an equality comparison between Fellows
+        """
+        f1 = fellow.Fellow("Joshua", "Kimani")
+        f2 = fellow.Fellow("Joshua", "Kimani")
+        f1.unique_id = 344
+        f2.unique_id = 45
+        self.assertNotEqual(f1, f2)
+
+        f1.unique_id = 66
+        f2.unique_id = 66
+        self.assertEqual(f1, f2)

8376c4561bc9325931089348b8fcad4d688f7a25
Michael Kamau <michael.kamau@andela.com>
2016-12-01 12:49:50 +0300

    Add exception to aid in handling ambigious cases in user interactions

--- office_space_allocation/utilities.py
+++ office_space_allocation/utilities.py
@@ -1,8 +1,20 @@
 class RoomFullError(Exception):
     pass
 
+
 class InvalidRoomOccupantError(Exception):
     pass
 
+
 class MultiplePeopleFoundException(Exception):
-	pass
+    pass
+
+
+class AmbiguityException(Exception):
+    """
+    This exception is raised to indicate a case of ambiguity when interacting
+    with input from the user.
+
+    Raise it and catch it to seek clarity for the user,
+    """
+    pass
